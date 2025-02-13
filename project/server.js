3eeec4576cf1b93ac075a32889fb0f66c74cd939
N0153128 <noisebro@hotmail.com>
2025-02-13 20:07:29 +0000

    restructured to tailor current setup

---
+++ project/server.js
@@ -0,0 +1,69 @@
+console.log('WS server has started');
+
+let WebSocket = require('ws');
+let server = new WebSocket.Server({port: 8282});
+
+function heartbeat() {
+    this.isAlive = true;
+}
+
+
+function broadcast(msg) {
+    console.log(`Received: ${msg}`);
+    server.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(msg);
+        }
+     });
+ };
+
+
+
+
+
+ server.username = function() {
+    let noun = ['Bunny', 'Kitten', 'Ram', 'Wolf', 'Fox'];
+    let adjective = ['Wise', 'Smart', 'Clever', 'Bright', 'Creative'];
+
+    function makeID() {
+        let username = [];
+        username.push(adjective[Math.floor(Math.random()*adjective.length)]);
+        username.push(noun[Math.floor(Math.random()*noun.length)]);
+        username = username.toString().replace(/,/g, ' ');
+        return username;
+    };
+    return makeID;
+}
+
+server.on('connection', function connection(wss, req) {
+    wss.id = server.username();
+    wss.isAlive = true;
+    const ip = req.socket.remoteAddress;
+    let interval = setInterval( function ping() {
+        server.clients.forEach(function each(wss) {
+            if (wss.isAlive === false) return wss.terminate();
+            wss.isAlive = false;
+            wss.ping();
+        });
+    }, 5000);
+    wss.on('pong', heartbeat);
+    wss.on('error', console.error);
+    broadcast('client connected')
+    console.log('client connected')
+
+    wss.on('message', function incoming(message) {
+        // message.toJSON;
+        // let clientMessage = JSON.parse(message).message;
+        // let prop = `${wss.id}: ${clientMessage}`
+        message.toJSON;
+        let cleintMessage = JSON.parse(message).message;
+        let prop = `${wss.id}: ${cleintMessage}`;
+        console.log(message);
+        broadcast(prop)
+    });
+
+    server.on('close', function() {
+        console.log('Client disconnected');
+        broadcast('Client Disconnected')
+    });
+});
